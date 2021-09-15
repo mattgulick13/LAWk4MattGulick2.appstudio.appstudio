f09e240d59da9e62704341d5e69f91f4cff1daf3
mattgulick13 <90654494+mattgulick13@users.noreply.github.com>
2021-09-15 16:34:48 -0500

    Initial commit

---
+++ forms/pets/pets.js
@@ -0,0 +1,12 @@
+let petNames = ["Darcy", "Jack", "Jerry", "Marty", "Vito", "Vinny", "Riley", "Saddie", "Marge", "Sam"]
+
+let withSal = petNames
+withSal.push("Sal")
+
+console.log(`The third pet is named ${petNames[2]}.`)
+
+console.log(`The array has ${withSal.length} in it.`)
+
+//withSal = ["Darcy", "Jack", "Jerry", "Marty", "Vito", "Vinny", "Riley", "Saddie", "Marge", "Sam", "Sal"]
+
+console.log(withSal.pop())
