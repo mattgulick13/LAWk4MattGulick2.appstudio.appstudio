d5233c7f0c5a5d5f0d2906544a394275b2f2d17d
mattgulick13 <90654494+mattgulick13@users.noreply.github.com>
2021-09-15 16:38:46 -0500

    Example

--- forms/pets/pets.js
+++ forms/pets/pets.js
@@ -10,3 +10,5 @@ console.log(`The array has ${withSal.length} in it.`)
 //withSal = ["Darcy", "Jack", "Jerry", "Marty", "Vito", "Vinny", "Riley", "Saddie", "Marge", "Sam", "Sal"]
 
 console.log(withSal.pop())
+
+console.log(`hello`)
